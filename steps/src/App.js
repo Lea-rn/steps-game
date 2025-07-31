2eb7f538091f7ab61513cde6b5a5739976da7393
achrefmechergui <achreffcb123achref@gamil.com>
2025-07-31 19:53:13 +0100

    onClick

---
+++ steps/src/App.js
@@ -0,0 +1,49 @@
+const messages = [
+  "Learn React",
+  "Apply for jobs 👜",
+  "Invest your new incomes 🤑",
+];
+
+export default function App() {
+  const step = 1;
+   
+
+  function handlePrevious (){
+    alert ("previous")
+  }
+
+  function handleNext (){
+    alert("next")
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step} : {messages[step - 1]}{" "}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+         
+        >
+          Previous
+        </button>
+        <button
+        //   onClick={() => alert("next")}
+        onClick={handleNext}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
